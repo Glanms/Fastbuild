ea32098c00d83499848545c3cee5f4729d82af09
Glanms <guojq0ms@163.com>
2016-04-08 10:49:19 +0800

    更新gradle为2.0.0

--- app/src/main/java/com/gjsk/fastbuild/base/BaseActivity.java
+++ app/src/main/java/com/gjsk/fastbuild/base/BaseActivity.java
@@ -3,16 +3,64 @@ package com.gjsk.fastbuild.base;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
+
 /**
  * Created by Glanms on 16/4/5.
  */
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //初始化注入
+        ButterKnife.bind(this);
+        initInjection();
+        initView();
     }
 
+    /**
+     * 初始化控件
+     */
+    public abstract void initView();
+
+    /**
+     * 初始化注入
+     */
+    public abstract void initInjection();
+
+    /**
+     * 显示默认加载界面
+     */
+    public void showLoading() {
+
+    }
+
+    /**
+     * 隐藏加载界面
+     */
+    public void hideLoading() {
+
+    }
+
+    /**
+     * 显示默认错误界面
+     */
+    public void showErrorView() {
+
+    }
+
+    /**
+     * 隐藏错误界面
+     */
+    public void hideErrorView() {
+
+    }
+
+
+    public void showConfirmDialog() {
+    }
 
 }
--- app/src/main/java/com/gjsk/fastbuild/base/BaseFragment.java
+++ app/src/main/java/com/gjsk/fastbuild/base/BaseFragment.java
@@ -5,11 +5,13 @@ import android.content.Intent;
 /**
  * Created by Glanms on 16/4/5.
  */
-public class BaseFragment extends android.support.v4.app.Fragment {
+public abstract class BaseFragment extends android.support.v4.app.Fragment {
 
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+
 }
